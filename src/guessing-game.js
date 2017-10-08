fed65f5081f8324c129f3bff5a0ced6eafc78880
Dmitry Popov <Dimapopov.ru@yandex.ru>
2017-10-08 12:34:31 +0300

    100 test commit

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this.min = 0;
+    	this.max = 0;
+    }
 
     setRange(min, max) {
-
+    	this.min = min;
+    	this.max = max;
     }
 
     guess() {
-
+    	this.guessNumb = Math.round((this.max - this.min)/ 2) + this.min;
+    	return this.guessNumb;
     }
 
     lower() {
-
+    	this.max = Math.round((this.max - this.min)/ 2) + this.min;
     }
 
     greater() {
-
+    	this.min = Math.round((this.max - this.min)/ 2) + this.min;
     }
 }
 
